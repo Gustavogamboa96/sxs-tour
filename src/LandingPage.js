aec80a4447533d6cea5560332c0a129950baab4d
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2024-08-01 10:27:45 -0400

    mobile view fixed

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,10 +3,10 @@ import './LandingPage.css';
 
 
 const links = [
-    { href: 'https://www.youtube.com/@lavidabohemev0', text: 'Link 1' },
-    { href: 'https://open.spotify.com/artist/5gs7iemsrjIJbz0ryFcy79', text: 'Link 2' },
-    { href: 'https://lavidaboheme.bandcamp.com/', text: 'Link 3' },
-    { href: 'https://www.instagram.com/lavidaboheme/', text: 'Link 4' }
+    { href: 'https://www.youtube.com/@lavidabohemev0', text: 'https://youtube.com' },
+    { href: 'https://open.spotify.com/artist/5gs7iemsrjIJbz0ryFcy79', text: 'https://spotify.com' },
+    { href: 'https://lavidaboheme.bandcamp.com/', text: 'https://bandcamp.com' },
+    { href: 'https://www.instagram.com/lavidaboheme/', text: 'https://instagram.com' }
   ];
 
 const events = [
