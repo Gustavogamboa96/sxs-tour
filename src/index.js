89e2af57e1608b42b5ffc3508fb47084e2d6af9f
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2024-08-02 21:42:53 -0400

    added vercel analytics fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import LandingPage from './LandingPage';
-import {Analytics} from '@vercel/analytics';
+import {inject} from '@vercel/analytics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LandingPage />
-    <Analytics />
+    {inject()}
   </React.StrictMode>
 );
 
