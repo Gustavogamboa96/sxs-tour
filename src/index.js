c5b4aca7ff75bcce3fecc1f4f4b18a5fdbac5345
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2024-08-02 21:38:11 -0400

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import LandingPage from './LandingPage';
+import {Analytics} from '@vercel/analytics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LandingPage />
+    <Analytics />
   </React.StrictMode>
 );
 
