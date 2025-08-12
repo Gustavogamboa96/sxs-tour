e189177ae994abdbb8f87d8022807435cb863756
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2025-08-12 12:22:15 -0400

    entretenimiento text video

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -66,7 +66,7 @@ export default function LandingPage() {
                     playsInline
                     className="background-video"
                 >
-                    <source src="/images/MuchachosAfueraTeaser1.webm" type="video/webm" />
+                    <source src="/images/LOOP3Contexto.webm" type="video/webm" />
                 </video>
                 <div className="container" style={{ position: 'relative', zIndex: 1 }}>
                     <div className='flex-grow-1 music-player-margin-bottom'>
