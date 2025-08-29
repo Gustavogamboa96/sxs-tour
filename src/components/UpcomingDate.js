d559427d4ead0412c66379c7beefa47e3f5360ab
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2025-08-29 15:25:55 -0400

    add miami link

--- src/components/UpcomingDate.js
+++ src/components/UpcomingDate.js
@@ -11,7 +11,7 @@ import './Signup.css'
 
 
 export default function UpcomingDate(props) {
-  const {handleCloseUpcomingDate, handleOpenUpcomingDate, openUpcomingDate, openSignup} = props;
+  const { handleCloseUpcomingDate, handleOpenUpcomingDate, openUpcomingDate, openSignup } = props;
   // const [open, setOpen] = useState(false);
   // const [email, setEmail] = React.useState('');
   // const [error, setError] = React.useState(false);
@@ -36,33 +36,33 @@ export default function UpcomingDate(props) {
     '&:hover': {
       color: '#1a1c1a',
       backgroundColor: '#3fea4b', // Adjust hover color if needed
-      opacity: 0.8, 
+      opacity: 0.8,
     },
     borderRadius: 5, // Adjust border radius as desired
     padding: '12px 24px', // Adjust padding as desired
     fontSize: '16px',
     fontWeight: 600,
     textTransform: 'uppercase',
-    transition: 'all 0.2s ease-in-out', 
+    transition: 'all 0.2s ease-in-out',
   }));
 
-    const style = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: 425,
-      bgcolor: 'background.paper',
-      // border:  '2px solid #000',
-      boxShadow: 24,
-      p: 4,
-      color: '#B71C1C',
-      backgroundColor: '#212121',
-      outline: 'none',
-      fontFamily:'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 425,
+    bgcolor: 'background.paper',
+    // border:  '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    color: '#B71C1C',
+    backgroundColor: '#212121',
+    outline: 'none',
+    fontFamily: 'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
 
-    };
-// Component mounts ready to be shown by parent
+  };
+  // Component mounts ready to be shown by parent
 
   return (
     <div>
@@ -93,16 +93,16 @@ export default function UpcomingDate(props) {
               />
           
             </CoverImage> */}
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <img
-                        src="https://i.imgur.com/d1Bc4fP.jpeg"
-                        alt="Event Poster"
-                        style={{ width: '60%', height: 'auto' }}
-                    />
-                </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <img
+                src="https://i.imgur.com/d1Bc4fP.jpeg"
+                alt="Event Poster"
+                style={{ width: '60%', height: 'auto' }}
+              />
+            </div>
             {/* <img src="https://wzeweb-p-visuelorga-evn-affiche.s3.eu-west-1.amazonaws.com/affiche_1294214.png" alt="Event Poster" style={{ width: '50%',  height: 'auto'}} /> */}
             <div style={{ marginTop: '5vh', display: 'flex', justifyContent: 'center' }}>
-            {/* <StyledButton variant="contained" disableElevation
+              {/* <StyledButton variant="contained" disableElevation
             component="a" // This makes the Button act like an anchor tag
             href="https://tinyurl.com/bdjc393u" // The link destination
             target="_blank" // Opens the link in a new tab (optional)
@@ -112,42 +112,61 @@ export default function UpcomingDate(props) {
               }}>
                 Entradas
             </StyledButton> */}
-            <Button variant="contained" color="primary"
-            component="a" // This makes the Button act like an anchor tag
-            href='https://www.eventim-light.com/es/a/68235206e8423f40cbf39bca/e/682355e9e8423f40cbf39bcf' // The link destination
-            target="_blank" // Opens the link in a new tab (optional)
-              sx={{
-                fontFamily:'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
-                backgroundColor: '#B71C1C',
-                color: 'black',
-                width: '100%', // Adjust the width as needed
-                margin: 'auto', // Center the TextField horizontally
-                '&:hover': {
-                  backgroundColor: 'black',
-                  color: '#B71C1C',
-                },
-              }}>
-              Tickets Madrid 26.11.25
-          </Button>
-</div>
+              <Button variant="contained" color="primary"
+                component="a" // This makes the Button act like an anchor tag
+                href='https://www.eventim-light.com/es/a/68235206e8423f40cbf39bca/e/682355e9e8423f40cbf39bcf' // The link destination
+                target="_blank" // Opens the link in a new tab (optional)
+                sx={{
+                  fontFamily: 'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
+                  backgroundColor: '#B71C1C',
+                  color: 'black',
+                  width: '100%', // Adjust the width as needed
+                  margin: 'auto', // Center the TextField horizontally
+                  '&:hover': {
+                    backgroundColor: 'black',
+                    color: '#B71C1C',
+                  },
+                }}>
+                Tickets Madrid 26.11.25
+              </Button>
+            </div>
             <div style={{ marginTop: '5vh', display: 'flex', justifyContent: 'center' }}>
-          <Button variant="contained" color="primary"
-            component="a"
-            href='https://dice.fm/event/53n3w8-la-vida-boheme-28th-nov-la-2-de-apolo-barcelona-tickets?pid=530cb286&_branch_match_id=1060721144327835601&utm_medium=partners_api&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1a9MsTQxT0lONTVItbQvyEyxNTU2SE4ysjBTqytKTUstKsrMS49PKsovL04tsnXOKMrPTQUAw7GvMEgAAAA%3D'
-            target="_blank"
-              sx={{
-                fontFamily:'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
-                backgroundColor: '#B71C1C',
-                color: 'black',
-                width: '100%',
-                margin: 'auto',
-                '&:hover': {
-                  backgroundColor: 'black',
-                  color: '#B71C1C',
-                },
-              }}>
-              Tickets Barcelona 28.11.25
-          </Button>
+              <Button variant="contained" color="primary"
+                component="a"
+                href='https://dice.fm/event/53n3w8-la-vida-boheme-28th-nov-la-2-de-apolo-barcelona-tickets?pid=530cb286&_branch_match_id=1060721144327835601&utm_medium=partners_api&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1a9MsTQxT0lONTVItbQvyEyxNTU2SE4ysjBTqytKTUstKsrMS49PKsovL04tsnXOKMrPTQUAw7GvMEgAAAA%3D'
+                target="_blank"
+                sx={{
+                  fontFamily: 'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
+                  backgroundColor: '#B71C1C',
+                  color: 'black',
+                  width: '100%',
+                  margin: 'auto',
+                  '&:hover': {
+                    backgroundColor: 'black',
+                    color: '#B71C1C',
+                  },
+                }}>
+                Tickets Barcelona 28.11.25
+              </Button>
+            </div>
+            <div style={{ marginTop: '5vh', display: 'flex', justifyContent: 'center' }}>
+              <Button variant="contained" color="primary"
+                component="a"
+                href='https://shotgun.live/es/events/zeyzey-presents-la-vida-boheme'
+                target="_blank"
+                sx={{
+                  fontFamily: 'IBM Plex Sans, sans-serif, -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
+                  backgroundColor: '#B71C1C',
+                  color: 'black',
+                  width: '100%',
+                  margin: 'auto',
+                  '&:hover': {
+                    backgroundColor: 'black',
+                    color: '#B71C1C',
+                  },
+                }}>
+                Tickets Miami 20.12.25
+              </Button>
             </div>
           </Box>
         </Box>
